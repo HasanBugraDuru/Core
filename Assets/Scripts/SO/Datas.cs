d7fe8e5fca530a228cc223c446e0480809010acf
Hasan Buğra Duru <hasanbugraduru@hotmail.com>
2024-07-15 23:11:55 +0300

    First

---
+++ Assets/Scripts/SO/Datas.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Datas", menuName = " Datas")]
+
+public class Datas : ScriptableObject
+{
+    public float MusicValue;
+    public float SoundValue;
+}
